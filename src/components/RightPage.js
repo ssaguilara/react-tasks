1bc3089858d10002c377248b98f1f338f66d8291
Sergio Aguilar <ssaguilara3@gmail.com>
2023-08-28 12:39:12 -0500

    Quinto Commit: Se hace uso de localstorage para guardar informacion de los tasks

--- src/components/RightPage.js
+++ src/components/RightPage.js
@@ -6,18 +6,33 @@ import "./RightPage.css";
 import { useState } from "react";
 
 export default function RightPage() {
-  const defaultTasks = [
-    { text: "First Task ", completed: true },
-    { text: "Second Task", completed: false },
-    { text: "Third Task", completed: false },
-    { text: "Fourth Task", completed: false },
-    { text: "Fifth Task", completed: true },
-    { text: "Sixth Task", completed: false },
-    { text: "Seventh Task", completed: false },
-    { text: "Eighth Task", completed: false },
-  ];
+  // const defaultTasks = [
+  //   { text: "First Task ", completed: true },
+  //   { text: "Second Task", completed: false },
+  //   { text: "Third Task", completed: false },
+  //   { text: "Fourth Task", completed: false },
+  //   { text: "Fifth Task", completed: true },
+  //   { text: "Sixth Task", completed: false },
+  //   { text: "Seventh Task", completed: false },
+  //   { text: "Eighth Task", completed: false },
+  // ];
 
-  const [tasks, setTasks] = useState(defaultTasks);
+  // localStorage.setItem("TASKS", JSON.stringify(defaultTasks));
+  // localStorage.removeItem('TASKS');
+
+  const localStorageTasks = localStorage.getItem("TASKS");
+
+  let parsedTasks;
+
+  if (localStorageTasks) {
+    parsedTasks = JSON.parse(localStorageTasks);
+  } else {
+    localStorage.setItem("TASKS", JSON.stringify([]));
+    parsedTasks = [];
+  }
+
+  const [tasks, setTasks] = useState(parsedTasks);
+
   const completedTask = tasks.filter((task) => task.completed).length;
   const totalTasks = tasks.length;
 
@@ -25,20 +40,24 @@ export default function RightPage() {
   const searchedTask = tasks.filter((task) =>
     task.text.toLowerCase().includes(searchValue.toLowerCase())
   );
- 
+
+  const saveTasks = (newTasks) => {
+    localStorage.setItem("TASKS", JSON.stringify(newTasks));
+    setTasks(newTasks);
+  };
+
   const completeTask = (index) => {
     const newTasks = [...tasks];
     newTasks[index].completed = !newTasks[index].completed;
-    setTasks(newTasks);
+    saveTasks(newTasks);
   };
 
   const deleteTask = (index) => {
     const newTasks = [...tasks];
     newTasks.splice(index, 1);
-    setTasks(newTasks);
+    saveTasks(newTasks);
   };
 
- 
   return (
     <div className="rightPage">
       <Counter completed={completedTask} total={totalTasks} />
@@ -46,8 +65,13 @@ export default function RightPage() {
 
       <TaskList>
         {searchedTask.map((task, index) => (
-          <TaskItem key={index} text={task.text} completed={task.completed} onCompleted={() => completeTask(index)}
-          onDelete={() => deleteTask(index)} />
+          <TaskItem
+            key={index}
+            text={task.text}
+            completed={task.completed}
+            onCompleted={() => completeTask(index)}
+            onDelete={() => deleteTask(index)}
+          />
         ))}
       </TaskList>
     </div>
--- src/components/RightPage/TaskList.js
+++ src/components/RightPage/TaskList.js
@@ -1,7 +1,7 @@
 import './TaskList.css'
 
 export function TaskList(props) {
-    console.log(props.children)
+    // console.log(props.children)
     return (
       <ul className="taskList">
         {props.children}
